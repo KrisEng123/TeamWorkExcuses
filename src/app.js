90d4a68371cdc890b5cd2d7a0fb8bb1321f8160a
AOP <alex@vki.es>
2021-12-10 00:10:56 +0000

    classroom 081221

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log(Math.floor(Math.random() * who.length));
+  let excuse = console.log(
+    who[Math.floor(Math.random() * who.length)] +
+      " " +
+      action[Math.floor(Math.random() * action.length)] +
+      " " +
+      what[Math.floor(Math.random() * what.length)] +
+      " " +
+      when[Math.floor(Math.random() * when.length)]
+  );
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
